546d588dcf532fe5964ec53287d0f881f2b7aa2a
LucaBasilico <luca.basilico@declab.it>
2016-07-31 17:26:46 +0200

    sass-gulp integration set up environment

---
+++ Gulpfile.js
@@ -0,0 +1,9 @@
+//set up the tasks by classic syntaxs'node
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
